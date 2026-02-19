5091e90723d0d9a55581899a39baaaadcb61e58d
Jasvinder Singh <jsvndrsnghjss@gmail.com>
2026-02-19 13:46:59 +0530

    Time: 9 ms (63.41%), Space: 48 MB (57.07%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,35 +1,33 @@
-import java.util.Arrays;
-
 class Solution {
 
-    public boolean canFinish(int[] piles, int speed, int h) {
-        long hours = 0;
-
-        for (int bananas : piles) {
-            hours += (bananas + speed - 1) / speed;
-        }
-
-        return hours <= h;
+    public int check(int[]piles, int mid, int h){
+        long totalHrs = 0;
+        for(int i=0; i<piles.length; i++){
+            totalHrs += (piles[i] + mid-1)/mid; 
+        } 
+        return totalHrs <= h ? 1 : 0;
     }
 
+
     public int minEatingSpeed(int[] piles, int h) {
+        
+        int low =1;
+        int high= Arrays.stream(piles).max().getAsInt();
 
-        int low = 1;
-        int high = Arrays.stream(piles).max().getAsInt();
-        int ans = high;
+        int ans=1;
 
-        while (low <= high) {
+        while(low<=high){
+            int mid= (low+high)/2;
 
-            int mid = low + (high - low) / 2;
-
-            if (canFinish(piles, mid, h)) {
-                ans = mid;
-                high = mid - 1;
-            } else {
-                low = mid + 1;
+            if(check(piles, mid, h)==1){
+                ans=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
             }
         }
-
         return ans;
+
     }
 }
