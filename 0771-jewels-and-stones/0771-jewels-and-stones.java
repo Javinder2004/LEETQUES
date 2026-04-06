d2d46c392170bfaacf22f762bd4b116d83f2d0dd
Jasvinder Singh <jsvndrsnghjss@gmail.com>
2026-04-06 14:24:19 +0530

    Time: 0 ms (100%), Space: 43.7 MB (5.5%) - LeetHub

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+      int []freq=new int[256];
+      for(int i=0;i<jewels.length();i++){
+        char ch=jewels.charAt(i);
+        freq[ch]=1;
+      }
+      int count=0;
+      for(int i=0;i<stones.length();i++){
+        char ch=stones.charAt(i);
+        if(freq[ch]==1){
+            count++;
+        }
+        }
+        return count;
+    }
+}
