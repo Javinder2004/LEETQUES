5ead0a04c8ed44d681a5dae63fbb54a71e4c9cc8
Jasvinder Singh <jsvndrsnghjss@gmail.com>
2026-09-10 15:36:08 +0530

    Time: 1 ms (99.89%), Space: 55.8 MB (17.73%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,6 +1,25 @@
 class Solution {
     public int majorityElement(int[] nums) {
-       Arrays.sort(nums);
-        return nums[nums.length/2]; 
+    //    Arrays.sort(nums);
+    //     return nums[nums.length/2]; 
+//     }
+// }
+        int count = 0;
+        int cand = 0;
+
+        for (int x : nums) {
+
+            if (count == 0) {
+                cand = x;
+            }
+
+            if (x == cand) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        return cand;
     }
 }
